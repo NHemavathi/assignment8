cc58a24d9c5c8df87f5b292f351fa9930333e1d5
NHemavathi <hemavathin@sit.ac.in>
2025-05-22 17:09:10 +0530

    Create logout.py

---
+++ logout.py
@@ -0,0 +1,10 @@
+# accounts/urls.py
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
+
+urlpatterns = [
+    path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('profile/', views.profile, name='profile'),
+]
